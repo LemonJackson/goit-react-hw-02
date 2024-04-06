9bcc03fe352c3b133e95f3603210ba5c3efca1e9
LemonJackson <107397445+LemonJackson@users.noreply.github.com>
2024-04-07 00:40:37 +0300

    v1.00

---
+++ src/App/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import Description from '../Description/Description';
+import Notification from '../Notification/Notification';
+import Options from '../Options/Options';
+import Feedback from '../Feedback/Feedback';
+import './App.css';
+
+export default function App() {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  useEffect(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    if (savedFeedback) {
+      return setFeedback(JSON.parse(savedFeedback));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  return (
+    <div>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} />
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
+    </div>
+  );
+}
---
+++ src/Feedback/Feedback.jsx
@@ -0,0 +1,15 @@
+import css from './Feedback.module.css';
+
+export default function Feedback({ feedback, totalFeedback }) {
+  const percentagePositive = Math.round((feedback.good / totalFeedback) * 100);
+
+  return (
+    <ul>
+      <li>Good: {feedback.good}</li>
+      <li>Neutral: {feedback.neutral}</li>
+      <li>Bad: {feedback.bad}</li>
+      <li>Total: {totalFeedback} </li>
+      <li>Positive: {percentagePositive}%</li>
+    </ul>
+  );
+}
